52ba673b8f1fa33a4471cc5d200f3ad18b66ae76
Camilo Bolaños <camiloa.andresc@hotmail.com>
2025-02-24 01:17:17 -0500

    Implementación de nuevas páginas y configuración de enrutamiento en la aplicación Angular

--- 02-bases/src/app/app.component.ts
+++ 02-bases/src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  standalone: false,
 })
 export class AppComponent {
-  public title = 'Hola Mundo';
-  public counter = 1;
+  title = 'bases angular';
 }
