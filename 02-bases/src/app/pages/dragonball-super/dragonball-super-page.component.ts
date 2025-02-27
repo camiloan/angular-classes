7ff6ba4d1fa4948b640e52b598b4c3ae4d41ed57
Camilo Bolaños <camiloa.andresc@hotmail.com>
2025-02-26 23:18:43 -0500

    Añadir página Dragonball Super y mejorar la funcionalidad de agregar personajes en la página Dragonball

--- 02-bases/src/app/pages/dragonball-super/dragonball-super-page.component.ts
+++ 02-bases/src/app/pages/dragonball-super/dragonball-super-page.component.ts
@@ -7,20 +7,17 @@ interface Character {
 }
 
 @Component({
-  selector: 'app-dragonball',
-  imports: [],
-  templateUrl: './dragonball-page.component.html',
+  selector: 'app-dragonball-super',
+  templateUrl: './dragonball-super-page.component.html',
 })
-export class DragonballPageComponent {
+export class DragonballSuperPageComponent {
 
   name = signal('');
   power = signal(0)
 
   characters = signal<Character[]>([
     { id: 1, name: 'Goku', power: 9001 },
-    /*     { id: 2, name: 'Vegeta', power: 8000 },
-        { id: 3, name: 'Piccolo', power: 3000 },
-        { id: 4, name: 'Yamcha', power: 500 } */
+    { id: 2, name: 'Vegeta', power: 8000 },
   ])
 
   addCharacter() {
@@ -39,7 +36,11 @@ export class DragonballPageComponent {
     )
 
     this.resetFields();
+
+
+
   }
+
   resetFields() {
     this.name.set('')
     this.power.set(0)
