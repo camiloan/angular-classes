0ae34663fc408cca9b0a1489981b2db33f75de2e
Camilo Bolaños <camiloa.andresc@hotmail.com>
2025-03-02 21:01:56 -0500

    Añadir estructura inicial de la aplicación Gifs con componentes y configuración de estilos

---
+++ 03-gifs-app/src/app/gifs/components/side-menu/side-menu-options/side-menu-options.component.ts
@@ -0,0 +1,32 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+interface SideMenuOption {
+  icon: string;
+  label: string;
+  route: string
+  subLabel: string;
+}
+
+@Component({
+  selector: 'gifs-side-menu-options',
+  imports: [RouterLink, RouterLinkActive],
+  templateUrl: './side-menu-options.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class SideMenuOptionsComponent {
+  menuOptions: SideMenuOption[] = [
+    {
+      icon: 'fa-solid fa-chart-line',
+      label: 'Trending',
+      subLabel: 'Gifs Populares',
+      route: '/dashboard/trending'
+    },
+    {
+      icon: 'fa-solid fa-magnifying-glass',
+      label: 'Buscador',
+      subLabel: 'Buscar Gifs',
+      route: '/dashboard/search'
+    }
+  ]
+}
