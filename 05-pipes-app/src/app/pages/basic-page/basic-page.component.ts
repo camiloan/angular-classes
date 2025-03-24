5b5a29dae549467005acf1796f384208cbc07f71
Camilo Bolaños <camiloa.andresc@hotmail.com>
2025-03-24 15:57:28 -0500

    Añadir nuevos pipes de fecha y número en las páginas correspondientes, mejorar la visualización de datos y actualizar el manejo de fechas en el componente básico

--- 05-pipes-app/src/app/pages/basic-page/basic-page.component.ts
+++ 05-pipes-app/src/app/pages/basic-page/basic-page.component.ts
@@ -1,9 +1,9 @@
-import { LowerCasePipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
-import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { DatePipe, LowerCasePipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
+import { ChangeDetectionStrategy, Component, effect, signal } from '@angular/core';
 
 @Component({
   selector: 'app-basic-page',
-  imports: [LowerCasePipe, UpperCasePipe, TitleCasePipe],
+  imports: [LowerCasePipe, UpperCasePipe, TitleCasePipe, DatePipe],
   templateUrl: './basic-page.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
@@ -11,4 +11,15 @@ export default class BasicPageComponent {
   nameLower = signal('camilo')
   nameUpper = signal('Camilo')
   fullName = signal('cAmIlO BoLaÑoS')
+  customDate = signal(new Date())
+
+
+  tickingDateEffect = effect((onCleanup) => {
+    const intervaL = setInterval(() => {
+      this.customDate.set(new Date())
+    }, 1000);
+    onCleanup(() => {
+      clearInterval(intervaL);
+    });
+  })
 }
