dcedb5b4c8be9a3dfeabe798ce981ea794815477
Camilo Bolaños <camiloa.andresc@hotmail.com>
2025-03-24 16:08:34 -0500

    Registrar datos locales para español y añadir opciones de cambio de idioma en la página básica

--- 05-pipes-app/src/app/app.config.ts
+++ 05-pipes-app/src/app/app.config.ts
@@ -1,8 +1,20 @@
-import { type ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { type ApplicationConfig, LOCALE_ID, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { registerLocaleData } from '@angular/common';
+
+import localEs from '@angular/common/locales/es';
+import localFr from '@angular/common/locales/fr';
+import localEn from '@angular/common/locales/en';
+
+registerLocaleData(localEs, 'es-CO');
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes),
+  {
+    provide: LOCALE_ID,
+    useValue: 'es-CO'
+  }
+  ]
 };
