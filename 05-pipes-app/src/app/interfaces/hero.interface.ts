8cafeae0f5d9a7f0fdc93e525bd221f478ab4993
Camilo Bolaños <camiloa.andresc@hotmail.com>
2025-03-24 21:14:56 -0500

    Añadir página personalizada con soporte para ToggleCasePipe y datos de héroes, incluyendo tabla de visualización y lógica de señal

---
+++ 05-pipes-app/src/app/interfaces/hero.interface.ts
@@ -0,0 +1,28 @@
+// biome-ignore lint/style/useEnumInitializers: <explanation>
+export enum Color {
+  red,
+  black,
+  blue,
+  green,
+}
+
+// biome-ignore lint/style/useEnumInitializers: <explanation>
+export enum Creator {
+  DC,
+  Marvel,
+}
+
+export interface Hero {
+  id: number;
+  name: string;
+  canFly: boolean;
+  color: Color;
+  creator: Creator;
+}
+
+export const ColorMap = {
+  [Color.red]: '#E57373',
+  [Color.black]: '#424242',
+  [Color.blue]: '#64B5F6',
+  [Color.green]: '#81C784',
+};
