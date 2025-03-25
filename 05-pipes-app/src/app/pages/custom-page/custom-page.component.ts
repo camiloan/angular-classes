b3c1400bfd90a744c0e7e1b58418191bccf9f144
Camilo Bolaños <camiloa.andresc@hotmail.com>
2025-03-24 22:18:33 -0500

    Añadir soporte para nuevos pipes: CanFlyPipe, HeroColorPipe, HeroTextColorPipe, HeroCreatorPipe y HeroSortByPipe; implementar lógica de ordenación y visualización en la página personalizada

--- 05-pipes-app/src/app/pages/custom-page/custom-page.component.ts
+++ 05-pipes-app/src/app/pages/custom-page/custom-page.component.ts
@@ -1,10 +1,17 @@
 import { Component, signal } from '@angular/core';
 import { ToggleCasePipe } from '../../pipes/toggle-case.pipe';
 import { heroes } from '../../data/heroes.data';
+import { CanFlyPipe } from '../../pipes/can-fly.pipe';
+import { HeroColorPipe } from '../../pipes/hero-color.pipe';
+import { HeroTextColorPipe } from '../../pipes/hero-text-color.pipe';
+import { TitleCasePipe } from '@angular/common';
+import { HeroCreatorPipe } from '../../pipes/hero-creator.pipe';
+import { HeroSortByPipe } from '../../pipes/hero-sort-by.pipe';
+import type { Hero } from '../../interfaces/hero.interface';
 
 @Component({
   selector: 'app-custom-page',
-  imports: [ToggleCasePipe],
+  imports: [ToggleCasePipe, CanFlyPipe, HeroColorPipe, HeroTextColorPipe, TitleCasePipe, HeroCreatorPipe, HeroSortByPipe],
   templateUrl: './custom-page.component.html',
 })
 export default class CustomPageComponent {
@@ -14,4 +21,6 @@ export default class CustomPageComponent {
   upperCase = signal(true)
 
   heroes = signal(heroes)
+
+  sortBy = signal<keyof Hero | null>(null)
 }
