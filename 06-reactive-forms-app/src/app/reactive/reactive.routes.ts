e67889820d38d5af5d7efd69561ca73e733b7701
Camilo Bolaños <camiloa.andresc@hotmail.com>
2025-03-25 00:00:14 -0500

    Añadir estructura básica de la aplicación con rutas y componentes iniciales; incluir configuración de Tailwind CSS y ajustes en el entorno de desarrollo

---
+++ 06-reactive-forms-app/src/app/reactive/reactive.routes.ts
@@ -0,0 +1,33 @@
+import type { Routes } from "@angular/router";
+import { DynamicPageComponent } from "./pages/dynamic-page/dynamic-page.component";
+import { BasicPageComponent } from "./pages/basic-page/basic-page.component";
+import { SwitchesPageComponent } from "./pages/switches-page/switches-page.component";
+
+export const authRoutes: Routes = [
+
+  {
+    path: '',
+    children: [
+      {
+        path: 'basic',
+        title: 'Básicos',
+        component: BasicPageComponent
+      }
+      ,
+      {
+        path: 'dynamic',
+        title: 'Dinámicos',
+        component: DynamicPageComponent
+      },
+      {
+        path: 'switches',
+        title: 'Switches',
+        component: SwitchesPageComponent
+      },
+      {
+        path: '**',
+        redirectTo: 'basic'
+      }
+    ]
+  }
+]
