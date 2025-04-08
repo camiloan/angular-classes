03864a7d5a4f1a24014efbdeb938eb8e58996aab
Camilo Bolaños <camiloa.andresc@hotmail.com>
2025-04-08 11:05:50 -0500

    feat: add product card component with routing to product details
    feat: implement front navbar with responsive design and routing
    
    feat: create store front layout to include navbar and router outlet
    
    feat: add gender page component with basic template
    
    feat: implement home page component displaying multiple product cards
    
    feat: add not found page component with basic template
    
    feat: create product page component with basic template
    
    feat: define store front routes for navigation between pages

---
+++ 09-teslo-shop/src/app/store-front/pages/home-page/home-page.component.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ProductCardComponent } from '@products/components/product-card/product-card.component';
+
+@Component({
+  selector: 'app-home-page',
+  imports: [ProductCardComponent],
+  templateUrl: './home-page.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class HomePageComponent { }
