c2fc45fec7d288603023507827ab29fc7510b796
Camilo Bolaños <camiloa.andresc@hotmail.com>
2025-04-10 20:58:57 -0500

    feat: enhance pagination component with active page tracking and routing

--- 09-teslo-shop/src/app/shared/components/pagination/pagination.component.ts
+++ 09-teslo-shop/src/app/shared/components/pagination/pagination.component.ts
@@ -1,16 +1,18 @@
-import { ChangeDetectionStrategy, Component, computed, input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, input, linkedSignal } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-pagination',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './pagination.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class PaginationComponent {
 
   pages = input(0);
-  currentPage = input(1);
+  currentPage = input<number>(1);
 
+  activePage =linkedSignal(this.currentPage);
   getPagesList = computed(() => {
     return Array.from({ length: this.pages() }, (_, i) => i + 1);
   })
