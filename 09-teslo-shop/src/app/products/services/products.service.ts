16010ac3c83c95af8a4f967afd21b147ac89b153
Camilo Bolaños <camiloa.andresc@hotmail.com>
2025-04-12 00:15:48 -0500

    feat: enhance product carousel and service with improved image handling and caching

--- 09-teslo-shop/src/app/products/services/products.service.ts
+++ 09-teslo-shop/src/app/products/services/products.service.ts
@@ -1,7 +1,7 @@
 import { inject, Injectable } from '@angular/core';
 import type { Product, ProductResponse } from '../interfaces/product.interface';
 import { HttpClient } from '@angular/common/http';
-import { type Observable, tap } from 'rxjs';
+import { delay, type Observable, of, tap } from 'rxjs';
 import { environment } from 'src/environments/environment';
 
 const baseUrl = environment.baseUrl;
@@ -15,20 +15,37 @@ interface Options {
 @Injectable({ providedIn: 'root' })
 export class ProductsService {
   private http = inject(HttpClient);
+  private productsCache = new Map<string, ProductResponse>();
+  private productCache = new Map<string, Product>();
 
   getProducts(options: Options): Observable<ProductResponse> {
     const { limit = 9, offset = 0, gender = '' } = options;
+
+    const key = `${limit}-${offset}-${gender}`;
+    if (this.productsCache.has(key)) {
+      // biome-ignore lint/style/noNonNullAssertion: <explanation>
+      return of(this.productsCache.get(key)!);
+    }
     return this.http.get<ProductResponse>(`${baseUrl}/products`, {
       params: {
         limit,
         offset,
         gender
       }
-    }).pipe(tap(resp => console.log(resp)));
+    }).pipe(tap(resp => this.productsCache.set(key, resp)));
   }
 
   getProductByIdSlug(idSlug: string): Observable<Product> {
-    return this.http.get<Product>(`${baseUrl}/products/${idSlug}`);
+
+    const key = `${idSlug}`;
+    if (this.productCache.has(key)) {
+      // biome-ignore lint/style/noNonNullAssertion: <explanation>
+      return of(this.productCache.get(key)!);
+    }
+
+    return this.http.get<Product>(`${baseUrl}/products/${idSlug}`).pipe(
+      delay(2000),
+      tap(resp => this.productCache.set(key, resp)));
   }
 
 }
