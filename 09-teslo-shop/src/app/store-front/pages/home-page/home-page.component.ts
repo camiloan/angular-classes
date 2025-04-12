52ee9d82f16e9c702a39640117a6ffbcddb9cda5
Camilo Bolaños <camiloa.andresc@hotmail.com>
2025-04-11 23:57:57 -0500

    feat: update Angular dependencies and implement pagination service for active page tracking

--- 09-teslo-shop/src/app/store-front/pages/home-page/home-page.component.ts
+++ 09-teslo-shop/src/app/store-front/pages/home-page/home-page.component.ts
@@ -1,27 +1,33 @@
 import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
 import { ProductCardComponent } from '@products/components/product-card/product-card.component';
 import { ProductsService } from '@products/services/products.service';
-
 import { rxResource } from '@angular/core/rxjs-interop';
 import { PaginationComponent } from '@shared/components/pagination/pagination.component';
+import { PaginationService } from '@shared/components/pagination/pagination.service';
 
 @Component({
   selector: 'app-home-page',
-  imports: [ProductCardComponent, PaginationComponent],
+  imports: [ProductCardComponent, PaginationComponent ],
   templateUrl: './home-page.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HomePageComponent {
 
   productsService = inject(ProductsService);
+  paginationService = inject(PaginationService);
+
+
+  /*  activatedRoute = inject(ActivatedRoute);
+   // biome-ignore lint/style/noNonNullAssertion: <explanation>
+   currentPage = toSignal(this.activatedRoute.queryParamMap.pipe(map(params => params.get('page') ? +params.get('page')! : 1), map(page => (Number.isNaN(page) ? 1 : page))), { initialValue: 1 }); */
 
 
 
 
   productsResource = rxResource({
-    request: () => ({}),
+    request: () => ({ page: this.paginationService.currentPage() - 1 }),
     loader: ({ request }) => {
-      return this.productsService.getProducts({})
+      return this.productsService.getProducts({ offset: request.page * 9 })
     },
   })
 }
