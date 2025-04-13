d1cbe114366213f393466811d6fe679d66e47521
Camilo Bolaños <camiloa.andresc@hotmail.com>
2025-04-13 00:04:50 -0500

    feat: implement login functionality with reactive form and error handling

--- 09-teslo-shop/src/app/auth/pages/login-page/login-page.component.ts
+++ 09-teslo-shop/src/app/auth/pages/login-page/login-page.component.ts
@@ -1,10 +1,41 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AuthService } from '@auth/services/auth.service';
 
 @Component({
   selector: 'app-login-page',
-  imports: [RouterLink],
+  imports: [RouterLink, ReactiveFormsModule],
   templateUrl: './login-page.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class LoginPageComponent { }
+export class LoginPageComponent {
+
+  fb = inject(FormBuilder);
+  hasError = signal(false);
+  isPosting = signal(false);
+
+  authService = inject(AuthService);
+
+  loginForm = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+  });
+
+  onSubmit() {
+    if (this.loginForm.invalid) {
+      this.hasError.set(true);
+      setTimeout(() => {
+        this.hasError.set(false);
+      }, 2000);
+      return;
+    }
+    const { email, password } = this.loginForm.value;
+    // biome-ignore lint/style/noNonNullAssertion: <explanation>
+    this.authService.login(email!, password!).subscribe((resp)=>{
+      console.log(resp);
+    });
+  }
+
+
+}
